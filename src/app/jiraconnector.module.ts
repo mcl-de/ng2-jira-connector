c34db4e4fd47d109960588c790d6b61ba91ab0a5
Pascal Temel <Pascal.Temel@mcl.de>
2017-05-02 09:39:42 +0200

    update build options

--- src/app/jiraconnector.module.ts
+++ src/app/jiraconnector.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { JiraconnectorService, JIRA_URL } from './jiraconnector.service';
 
 @NgModule({
-	declarations: [
-	],
+	declarations: [],
 	imports: [
-		BrowserModule,
 		HttpModule
 	],
-	providers: [
-		JiraconnectorService,
-		{ provide: JIRA_URL, useValue: 'https://jira.foo.bar/rest/api/2/' },
-	],
+	providers: [JiraconnectorService],
 	bootstrap: []
 })
 export class JiraconnectorModule { }
